91708e2079392f40c0a7252c42edc1a39817ce25
Matthew Heston <matthewheston@gmail.com>
2011-10-22 09:22:34 -0500

    Fix syntax error in tests

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,3 @@
 test("DatasetManager constructor", function() {
 	ok(new Socrata.DatasetManager("http://data.cityofchicago.org/Transportation/Towed-Vehicles/ygr5-vcbg"), "Constructor taking in URL");
-);
+});
