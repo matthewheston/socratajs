d5990645c459d0189cddb21790ba6954175d29d1
Matthew Heston <matthewheston@gmail.com>
2011-10-22 19:14:49 -0500

    Throw error if DatasetManager constructor isn't passed a URL

--- socrata.js
+++ socrata.js
@@ -1,38 +1,41 @@
 Socrata = (function(Socrata, $, undefined) {
 
-	//DatasetManager {{{
-	Socrata.DatasetManager = (function() {
-		function DatasetManager(url){
-			var that = this;
-			 extractUID(that, url);
-			 extractHost(that, url);
-		}
+  //DatasetManager {{{
+  Socrata.DatasetManager = (function() {
+    function DatasetManager(url){
+      if (url === undefined) {
+        throw new Error("DatasetManager constructor expects URL parameter");
+      }
+      var that = this;
+      extractUID(that, url);
+      extractHost(that, url);
+    }
 
 
-		//private methods {{{
-		extractUID = function(that, url) {
-			matches = url.match(/.*([a-z0-9]{4}-[a-z0-9]{4}).*/);
-			if ( matches == null || matches.length < 2 ) {
-				return false;
-			}
-			that.uid = matches[1];
-			return true;
-		};
+    //private methods {{{
+    extractUID = function(that, url) {
+      matches = url.match(/.*([a-z0-9]{4}-[a-z0-9]{4}).*/);
+      if ( matches == null || matches.length < 2 ) {
+        return false;
+      }
+      that.uid = matches[1];
+      return true;
+    };
 
-		extractHost = function(that, url) {
-			matches = url.match(/^(?:[^\/]+:\/\/)?([^\/]+)/im);
-			if ( matches == null || matches.length < 2 ) {
-				return;
-			}
-			this.host = "http://" + matches[1];
-		};
-		//}}}
+    extractHost = function(that, url) {
+      matches = url.match(/^(?:[^\/]+:\/\/)?([^\/]+)/im);
+      if ( matches == null || matches.length < 2 ) {
+        return;
+      }
+      this.host = "http://" + matches[1];
+    };
+    //}}}
 
-		return DatasetManager;
-	})();
-	//}}}
+    return DatasetManager;
+  })();
+  //}}}
 
-	return Socrata;
+  return Socrata;
 }(window.Socrata = window.Socrata || {}, jQuery));
 
 
--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,10 @@
 test("DatasetManager constructor", function() {
 	ok(new Socrata.DatasetManager("http://data.cityofchicago.org/Transportation/Towed-Vehicles/ygr5-vcbg"), "Constructor taking in URL");
+  raises(function() {
+    var datasetManager = new Socrata.DatasetManager();
+  },
+  function(e) {
+    return e.message == "DatasetManager constructor expects URL parameter";
+  },
+  "Constructor without URL parameter.");
 });
